daedfd08eeb8a8ed4bb1da4cabefd1b8cd0053e7
Davis Mariotti <davismariotti@gmail.com>
2018-04-23 18:33:46 -0700

    Start working on division

--- Jenny.srcs/sources_1/new/ALU.vhd
+++ Jenny.srcs/sources_1/new/ALU.vhd
@@ -19,7 +19,10 @@ signal product : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 signal quotient : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 signal modulus : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 signal m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+signal a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 signal d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+--signal q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+signal q : STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
 begin
 
 -- hardware inverter for 2's complement 
@@ -47,6 +50,64 @@ m13 <= (a(18 downto 0) & "0000000000000") when b(13) = '1';
 m14 <= (a(17 downto 0) & "00000000000000") when b(14) = '1';
 m15 <= (a(16 downto 0) & "000000000000000") when b(15) = '1';
 product <= m0 + m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + m11 + m12 + m13 + m14 + m15;
+
+
+-- Hardware divider
+d15 <= b(15 downto 0) & "0000000000000000";
+d14 <= b(14 downto 0) & "0000000000000000";
+d13 <= b(13 downto 0) & "0000000000000000";
+d12 <= b(12 downto 0) & "0000000000000000";
+d11 <= b(11 downto 0) & "0000000000000000";
+d10 <= b(10 downto 0) & "0000000000000000";
+a0 <= a;
+q(15) <= '1' when a0 > d15 else '0';
+a1 <= a0 - d15 when a0 > d15 else a0;
+
+q(14) <= '1' when a1 > d14 else '0';
+a2 <= a1 - d14 when a1 > d14 else a1;
+
+q(13) <= '1' when a2 > d13 else '0';
+a3 <= a2 - d13 when a2 > d13 else a2;
+
+q(12) <= '1' when a3 > d12 else '0';
+a4 <= a3 - d12 when a3 > d12 else a3;
+
+q(11) <= '1' when a4 > d11 else '0';
+a5 <= a4 - d11 when a4 > d11 else a4;
+
+q(10) <= '1' when a5 > d10 else '0';
+a6 <= a5 - d10 when a5 > d10 else a5;
+
+q(9) <= '1' when a6 > d9 else '0';
+a7 <= a6 - d9 when a6 > d9 else a6;
+
+q(8) <= '1' when a7 > d8 else '0';
+a8 <= a7 - d8 when a7 > d8 else a7;
+
+q(7) <= '1' when a8 > d7 else '0';
+a9 <= a8 - d7 when a8 > d15 else a8;
+
+q(6) <= '1' when a9 > d6 else '0';
+a10 <= a9 - d6 when a9 > d14 else a9;
+
+q(5) <= '1' when a10 > d5 else '0';
+a11 <= a10 - d5 when a10 > d13 else a10;
+
+q(4) <= '1' when a11 > d4 else '0';
+a12 <= a11 - d4 when a11 > d12 else a11;
+
+q(3) <= '1' when a12 > d3 else '0';
+a13 <= a12 - d3 when a12 > d11 else a12;
+
+q(2) <= '1' when a13 > d2 else '0';
+a14 <= a13 - d2 when a13 > d10 else a13;
+
+q(1) <= '1' when a14 > d1 else '0';
+a15 <= a14 - d1 when a14 > d9 else a14;
+
+q(0) <= '1' when a15 > d0 else '0';
+
+
 
 --begin hardware division
 --do something to calc quotient;
