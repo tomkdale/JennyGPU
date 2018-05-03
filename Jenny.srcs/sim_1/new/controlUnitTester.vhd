30d0a8acb30c4b8354fc164a89099f0b430e2a86
Andrew M Peacock <apeacock18@my.whitworth.edu>
2018-05-02 22:06:01 -0700

    control unit testbench

--- Jenny.srcs/sim_1/new/controlUnitTester.vhd
+++ Jenny.srcs/sim_1/new/controlUnitTester.vhd
@@ -9,7 +9,7 @@ end controlUnitTester;
 architecture Behavioral of controlUnitTester is
     component controlUnit is -- instruction memory
       port(reset:       in STD_LOGIC;
-        instr:          in STD_LOGIC_VECTOR(12 downto 0);
+        instr:          in STD_LOGIC_VECTOR(31 downto 0);
         CUreg0enable,CUreg1enable,CUreg2enable,CUreg3enable,CUbranch,CUbranchDataWrite:   out STD_LOGIC;
         CUimmCalc,CUbranchZero,CUload,CUdataMemWrite:                                     out STD_LOGIC;
         rot:            out     STD_LOGIC_VECTOR(1  downto 0);
@@ -17,7 +17,7 @@ architecture Behavioral of controlUnitTester is
       end component;
       
 signal reset: STD_LOGIC;
-signal instr: STD_LOGIC_VECTOR(12 downto 0);
+signal instr: STD_LOGIC_VECTOR(31 downto 0);
 signal CUreg0enable,CUreg1enable,CUreg2enable,CUreg3enable,CUbranch,CUbranchDataWrite: STD_LOGIC;
 signal CUimmCalc,CUbranchZero,CUload,CUdataMemWrite: STD_LOGIC;
 signal rot: STD_LOGIC_VECTOR(1  downto 0);
@@ -26,7 +26,7 @@ signal alucontrol: STD_LOGIC_VECTOR(3  downto 0);
 begin
 clkprocess :process
  begin
-    instr <= "1111111111111";
+    instr <= "11110010010000000000001000000001";
     wait for 10ns;
 
 end process;
