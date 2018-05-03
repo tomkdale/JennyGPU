bf5979d7053b2d7de3ba38d341c00d51fea4a7bf
Tom Dale <tom.k.dale@gmail.com>
2018-05-03 11:08:31 -0700

    FP3 mostly working

--- Jenny.srcs/sources_1/new/instructionMem.vhd
+++ Jenny.srcs/sources_1/new/instructionMem.vhd
@@ -57,7 +57,7 @@ architecture behave of imem is
     end function;
     
     -- Use the impure function to read RAM from a file and store in the FPGA's ram memory
-    signal mem: ramtype := InitRamFromFile("memfile2.dat");
+    signal mem: ramtype := InitRamFromFile("memfile1.dat");
     
     begin
     
