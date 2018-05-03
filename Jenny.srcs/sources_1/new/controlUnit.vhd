040b914b52657577eec9112e1ef4ea29603360fa
Thomas Dale <tom.k.dale@gmail.com>
2018-05-02 21:31:51 -0700

    edited instruction to be updated

--- Jenny.srcs/sources_1/new/controlUnit.vhd
+++ Jenny.srcs/sources_1/new/controlUnit.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity controlUnit is
     port(reset:     in STD_LOGIC;
-    instr:          in STD_LOGIC_VECTOR(12 downto 0);
+    instr:          in STD_LOGIC_VECTOR(31 downto 0);
     CUreg0enable,CUreg1enable,CUreg2enable,CUreg3enable,CUbranch,CUbranchDataWrite:   out STD_LOGIC;
           CUimmCalc,CUbranchZero,CUload,CUdataMemWrite:        out STD_LOGIC;
           rot:         out     STD_LOGIC_VECTOR(1  downto 0);
@@ -13,18 +13,22 @@ end controlUnit;
 architecture Behavioral of controlUnit is
 signal  allcontrol: STD_LOGIC_VECTOR(15 downto 0);
 begin
-allcontrol <= instr(12 downto 9) & "000000000010" when instr(8 downto 2) = "0000001";--vadd
-allcontrol <= instr(12 downto 9) & "000000001010" when instr(8 downto 2) = "0000010";--vsub
-allcontrol <= instr(12 downto 9) & "000000000011" when instr(8 downto 2) = "0000011";--vmult
-allcontrol <= instr(12 downto 9) & "000000000100" when instr(8 downto 2) = "0000100";--vdiv
-allcontrol <= instr(12 downto 9) & "000000000101" when instr(8 downto 2) = "0000110";--vmod
-allcontrol <= instr(12 downto 9) & "100100000110" when instr(8 downto 2) = "0000101";--blt
-allcontrol <= instr(12 downto 9) & "000000" & instr(1 downto 0) & "0111" when instr(8 downto 2) = "0000111";--rotate
-allcontrol <= "0000100000000000" when instr(8 downto 2) = "0001000";--jump
-allcontrol <= instr(12 downto 9) & "001000000010" when instr(8 downto 2) = "010XXXX";--vaddi
-allcontrol <= instr(12 downto 9) & "000010000111" when instr(8 downto 2) = "011XXXX";--loadv
-allcontrol <= instr(12 downto 9) & "000001000111" when instr(8 downto 2) = "100XXXX";--savev
-
+   process(instr) begin
+        case instr(27 downto 22) is
+           when  "000001" => allcontrol <= instr(31 downto 28) & "000000000010" ; --vadd
+           when  "000010" => allcontrol <= instr(31 downto 28) & "000000001010" ;--vsub
+           when  "000011" => allcontrol <= instr(31 downto 28) & "000000000011" ;--vmult
+           when  "000100" => allcontrol <= instr(31 downto 28) & "000000000100";--vdiv
+           when  "000101" => allcontrol <= instr(31 downto 28) & "000000000101";--vmod
+           when  "000110" => allcontrol <= instr(31 downto 28) & "100100000110";--blt
+           when  "000111" => allcontrol <= instr(31 downto 28) & "000000" & instr(1 downto 0) & "0111";--rotate
+           when  "001000" => allcontrol <= "0000100000000000";--jump
+           when  "001001" => allcontrol <= instr(31 downto 28) & "001000000010";--vaddi
+           when  "001010" => allcontrol <= instr(31 downto 28) & "000010000111";--loadv
+           when  "001011" => allcontrol <= instr(31 downto 28) & "000001000111";--savev
+           when others => allcontrol <= "0000000000000000";
+    end case;
+    end process;
  CUreg0enable<= allcontrol(15);
  CUreg1enable<=allcontrol(14);
  CUreg2enable<=allcontrol(13);
