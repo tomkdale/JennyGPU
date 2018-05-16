6eb384c682e5ef730321c5ac79e8654258cc387c
Davis Mariotti <davismariotti@gmail.com>
2018-05-16 11:16:30 -0700

    Hookup VGA output

--- Jenny.srcs/constrs_1/imports/Desktop/Nexys4DDR_Master.xdc
+++ Jenny.srcs/constrs_1/imports/Desktop/Nexys4DDR_Master.xdc
@@ -11,7 +11,7 @@ create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {cl
 
 ##Switches
 
-set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { reset }]; #IO_L24N_T3_RS0_15 Sch=sw[0]
+#set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { SW[0] }]; #IO_L24N_T3_RS0_15 Sch=sw[0]
 #set_property -dict { PACKAGE_PIN L16   IOSTANDARD LVCMOS33 } [get_ports { SW[1] }]; #IO_L3N_T0_DQS_EMCCLK_14 Sch=sw[1]
 #set_property -dict { PACKAGE_PIN M13   IOSTANDARD LVCMOS33 } [get_ports { SW[2] }]; #IO_L6N_T0_D08_VREF_14 Sch=sw[2]
 #set_property -dict { PACKAGE_PIN R15   IOSTANDARD LVCMOS33 } [get_ports { SW[3] }]; #IO_L13N_T2_MRCC_14 Sch=sw[3]
@@ -26,7 +26,7 @@ set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { reset 
 #set_property -dict { PACKAGE_PIN H6    IOSTANDARD LVCMOS33 } [get_ports { SW[12] }]; #IO_L24P_T3_35 Sch=sw[12]
 #set_property -dict { PACKAGE_PIN U12   IOSTANDARD LVCMOS33 } [get_ports { SW[13] }]; #IO_L20P_T3_A08_D24_14 Sch=sw[13]
 #set_property -dict { PACKAGE_PIN U11   IOSTANDARD LVCMOS33 } [get_ports { SW[14] }]; #IO_L19N_T3_A09_D25_VREF_14 Sch=sw[14]
-#set_property -dict { PACKAGE_PIN V10   IOSTANDARD LVCMOS33 } [get_ports { SW[15] }]; #IO_L21P_T3_DQS_14 Sch=sw[15]
+set_property -dict { PACKAGE_PIN V10   IOSTANDARD LVCMOS33 } [get_ports { resetSW }]; #IO_L21P_T3_DQS_14 Sch=sw[15]
 
 
 ## LEDs
--- Jenny.srcs/sources_1/new/jenny_top.vhd
+++ Jenny.srcs/sources_1/new/jenny_top.vhd
@@ -25,45 +25,50 @@ architecture Behavioral of jenny_top is
           CUbranch, CUbranchDataWrite, CUreg0enable, CUreg1enable, CUreg2enable, CUreg3enable: in STD_LOGIC;
           CUimmCalc, CUbranchZero, CUload, CUdataMemWrite: in STD_LOGIC;
           rot:        in STD_LOGIC_VECTOR(1 downto 0);
-          alucontrol: in STD_LOGIC_VECTOR(3 downto 0));
+          alucontrol: in STD_LOGIC_VECTOR(3 downto 0);
+          point1x,point2x,point3x,point4x,point5x,point6x,point7x,point8x: out STD_LOGIC_VECTOR(31 downto 0);
+          point1y,point2y,point3y,point4y,point5y,point6y,point7y,point8y: out STD_LOGIC_VECTOR(31 downto 0));
     end component;
 
     component controlUnit is
-            port(reset:     in STD_LOGIC;
-            instr:          in STD_LOGIC_VECTOR(31 downto 0);
-            CUreg0enable, CUreg1enable, CUreg2enable, CUreg3enable, CUbranch, CUbranchDataWrite: out STD_LOGIC;
-            CUimmCalc, CUbranchZero, CUload, CUdataMemWrite: out STD_LOGIC;
-            rot:        out STD_LOGIC_VECTOR(1 downto 0);
-            alucontrol: out STD_LOGIC_VECTOR(3 downto 0));
+        port(reset:      in  STD_LOGIC;
+             instr:      in  STD_LOGIC_VECTOR(31 downto 0);
+             CUreg0enable, CUreg1enable, CUreg2enable, CUreg3enable, CUbranch, CUbranchDataWrite: out STD_LOGIC;
+             CUimmCalc, CUbranchZero, CUload, CUdataMemWrite: out STD_LOGIC;
+             rot:        out STD_LOGIC_VECTOR(1 downto 0);
+             alucontrol: out STD_LOGIC_VECTOR(3 downto 0));
     end component;
     
     component imem is -- instruction memory
-          port(addr:  in  STD_LOGIC_VECTOR(15 downto 0);
-               instr: out STD_LOGIC_VECTOR(31 downto 0));
-     end component;
+        port(addr:  in  STD_LOGIC_VECTOR(15 downto 0);
+             instr: out STD_LOGIC_VECTOR(31 downto 0));
+    end component;
      
         -- The datapath needs a flip-flop register component for program counter etc.
-      component flipFlop generic(width: integer);
+    component flipFlop generic(width: integer);
         port(clk, reset: in  STD_LOGIC;
                       d: in  STD_LOGIC_VECTOR((width-1) downto 0);
                       q: out STD_LOGIC_VECTOR((width-1) downto 0));
-      end component;
+    end component;
       
-      component vga_out is
-         Port (clk: in STD_LOGIC;
-               resetSW: in STD_LOGIC;
-               hsync, vsync: out STD_LOGIC;
-               point1x,point2x,point3x,point4x,point5x,point6x,point7x,point8x: in STD_LOGIC_VECTOR(32 downto 0);
-               point1y,point2y,point3y,point4y,point5y,point6y,point7y,point8y: in STD_LOGIC_VECTOR(32 downto 0));
-        end component;
+    component VGAoutput is
+        port(clk:              in  STD_LOGIC;
+             resetSW:          in  STD_LOGIC;
+             hsync, vsync:     out STD_LOGIC;
+             red, green, blue: out STD_LOGIC_VECTOR(3 downto 0);
+             point1x,point2x,point3x,point4x,point5x,point6x,point7x,point8x: in STD_LOGIC_VECTOR(31 downto 0);
+             point1y,point2y,point3y,point4y,point5y,point6y,point7y,point8y: in STD_LOGIC_VECTOR(31 downto 0));
+    end component;
       
-     signal addr:     STD_LOGIC_VECTOR(15 downto 0);
-     signal addrnext: STD_LOGIC_VECTOR(15 downto 0);
-     signal instr:    STD_LOGIC_VECTOR(31 downto 0);
-     signal rot:      STD_LOGIC_VECTOR(1 downto 0);
-     signal CUimmCalc, CUbranchZero, CUload, CUdataMemWrite: STD_LOGIC;
-     signal CUreg0enable, CUreg1enable, CUreg2enable, CUreg3enable, CUbranch, CUbranchDataWrite :STD_LOGIC;
-     signal alucontrol: STD_LOGIC_VECTOR(3 downto 0);
+    signal addr:     STD_LOGIC_VECTOR(15 downto 0);
+    signal addrnext: STD_LOGIC_VECTOR(15 downto 0);
+    signal instr:    STD_LOGIC_VECTOR(31 downto 0);
+    signal rot:      STD_LOGIC_VECTOR(1 downto 0);
+    signal CUimmCalc, CUbranchZero, CUload, CUdataMemWrite: STD_LOGIC;
+    signal CUreg0enable, CUreg1enable, CUreg2enable, CUreg3enable, CUbranch, CUbranchDataWrite :STD_LOGIC;
+    signal alucontrol: STD_LOGIC_VECTOR(3 downto 0);
+    signal p1x, p2x, p3x, p4x, p5x, p6x, p7x, p8x: STD_LOGIC_VECTOR(31 downto 0);
+    signal p1y, p2y, p3y, p4y, p5y, p6y, p7y, p8y: STD_LOGIC_VECTOR(31 downto 0);
 begin
 
     cu: controlUnit port map(reset => reset, instr => instr, rot => rot, CUreg0enable => CUreg0enable,
@@ -75,11 +80,15 @@ begin
     dp: datapath port map(clk => clk, reset => reset, instr => instr, addr => addr, addrnext => addrnext,
         data0 => data0, data1 => data1, data2 => data2, data3 => data3, rot => rot, CUbranch => CUbranch, CUbranchDataWrite => CUbranchDataWrite, CUreg0enable => CUreg0enable,
         CUreg1enable => CUreg1enable, CUreg2enable => CUreg2enable, CUreg3enable => CUreg3enable, CUimmCalc => CUimmCalc, CUbranchZero => CUbranchZero, CUload => CUload,
-        CUdataMemWrite => CUdataMemWrite, alucontrol => alucontrol);
+        CUdataMemWrite => CUdataMemWrite, alucontrol => alucontrol,
+        point1x => p1x, point2x => p2x, point3x => p3x, point4x => p4x, point5x => p5x, point6x => p6x, point7x => p7x, point8x => p8x,
+        point1y => p1y, point2y => p2y, point3y => p3y, point4y => p4y, point5y => p5y, point6y => p6y, point7y => p7y, point8y => p8y);
     
-   pcCLK: flipFlop generic map(16) port map(clk => clk, reset => reset, d => addrnext, q => addr);
+    pcCLK: flipFlop generic map(16) port map(clk => clk, reset => reset, d => addrnext, q => addr);
 
- 
+    vga_output: VGAoutput port map(clk => clk, resetSW => resetSW, hsync => hsync, vsync => vsync, red => red, green => green, blue => blue,
+                point1x => p1x, point2x => p2x, point3x => p3x, point4x => p4x, point5x => p5x, point6x => p6x, point7x => p7x, point8x => p8x,
+                point1y => p1y, point2y => p2y, point3y => p3y, point4y => p4y, point5y => p5y, point6y => p6y, point7y => p7y, point8y => p8y);
  
  
 end Behavioral;
